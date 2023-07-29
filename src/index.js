35f961f8f31d28460d3de26bcd8d9293ee12e480
Milana <durckina.milana2016@yandex.ru>
2023-07-29 23:23:39 +0300

    new

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable linebreak-style */
+import './css/style.css';
+
+import './js/app';
+
+// TODO: write your code in app.js
